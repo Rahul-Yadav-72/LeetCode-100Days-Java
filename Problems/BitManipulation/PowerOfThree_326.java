31b62bd9909ab8f6a62662f0cd592fde08fa210e
Rahul Kumar Yadav <ry642499@gmail.com>
2026-07-24 00:26:20 +0530

    Day 1: Update LeetCode 231 and 326 solutions and add screenshots

--- Problems/BitManipulation/PowerOfThree_326.java
+++ Problems/BitManipulation/PowerOfThree_326.java
@@ -1,22 +1,26 @@
 public class PowerOfThree_326 {
+
+    // public boolean isPowerOfThree(int n) {
+    //     if (n <= 0) {
+    //         return false;
+    //     }
+
+    //     while (n % 3 == 0) {
+    //         n /= 3;
+    //     }
+
+    //     return n == 1;
+    // }
+
     public boolean isPowerOfThree(int n) {
-        if (n == 0) {
-            return false;
-        }
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        if (n == 1) {
-            return true;
-        }
-        return false;
+        return n > 0 && 1162261467 % n == 0;
     }
 
     public static void main(String[] args) {
         PowerOfThree_326 obj = new PowerOfThree_326();
-        System.out.println(obj.isPowerOfThree(27));
 
-        System.out.println(obj.isPowerOfThree(45));
+        System.out.println(obj.isPowerOfThree(27)); 
+        System.out.println(obj.isPowerOfThree(45)); 
+        
     }
-
 }
--- Problems/BitManipulation/PowerOfTwo_231.java
+++ Problems/BitManipulation/PowerOfTwo_231.java
@@ -1,21 +1,25 @@
 public class PowerOfTwo_231 {
+    // public boolean isPowerOfTwo(int n) {
+    //     if (n <= 0) {
+    //         return false;
+    //     }
+    //     while (n % 2 == 0) {
+    //         n /= 2;
+    //     }
+    //     return n == 1;
+    // }
+
     public boolean isPowerOfTwo(int n) {
-        if (n == 0) {
-            return false;
-        }
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        if (n == 1) {
-            return true;
-        }
-        return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 
     public static void main(String[] args) {
         PowerOfTwo_231 obj = new PowerOfTwo_231();
-        System.out.println(obj.isPowerOfTwo(16));
-        System.out.println(obj.isPowerOfTwo(15));
-    }
 
+        System.out.println(obj.isPowerOfTwo(27)); 
+        System.out.println(obj.isPowerOfTwo(16)); 
+        
+
+        
+    }
 }
