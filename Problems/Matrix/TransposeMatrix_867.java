de23d144e977f825a7eaf9b1589ef8f2b091d91b
Rahul Kumar Yadav <ry642499@gmail.com>
2026-08-18 17:41:31 +0530

    Add 1572 Matrix Diagonal Sum and 867 Transpose Matrix

---
+++ Problems/Matrix/TransposeMatrix_867.java
@@ -0,0 +1,22 @@
+public class TransposeMatrix_867 {
+    public static void main(String[] args){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] ans = transpose(matrix);
+        for(int i=0; i<ans.length; i++){
+            for(int j=0; j<ans[0].length; j++){
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] transpose(int[][] matrix){
+        int[][] ans = new int[matrix[0].length][matrix.length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+    
+}
